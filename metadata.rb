d279473ca1e082ce771fdc3ca5fa50e60d9f6f6e
Tom Harrison <tharrison@hugeinc.com>
2015-04-13 12:48:38 -0400

    Upgrade the IIS cookbook.
    - Moving from IIS 2.x to 4.x gets rid of ChefSpec deprecation notices
    which were flooding the console (#1).
    - Bumped the windows cookbook version because IIS 4.x requires windows
      >= 1.34.6.

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ description 'Installs/Configures Sitecore'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.4.0'
 
-depends 'windows', '~> 1.31'
+depends 'windows', '~> 1.36'
 depends 'powershell', '~> 3.0'
-depends 'iis', '~> 2.1'
+depends 'iis', '~> 4.1'
 depends 'openssl', '~> 1.1'
 depends 'git', '~> 4.0'
